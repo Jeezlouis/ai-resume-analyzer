8976f02a0e63d7d6723231557250a711a38183e1
jeezlouis <125540102+Jeezlouis@users.noreply.github.com>
2025-07-24 17:07:17 +0100

    authentication with puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,9 +2,12 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 
-export function meta({}: Route.MetaArgs) {
+export function meta({}: Route.MetaArgs) { 
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job" },
@@ -12,6 +15,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore()
+  const navigate = useNavigate()
+  
+  
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
     <section className="main-section">
